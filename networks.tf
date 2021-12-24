1e18ebbb6ef9749d5ecda059cf43432d6b8fb498
DeSemaS7 <desema.s7@gmail.com>
2021-12-24 23:05:23 +0300

    adding target_group to balancer mapping

--- networks.tf
+++ networks.tf
@@ -19,6 +19,17 @@ resource "yandex_lb_network_load_balancer" "balancer" {
       ip_version = "ipv4"
     }
   }
+  attached_target_group {
+    target_group_id = yandex_compute_instance_group.ig-1.load_balancer[0].target_group_id
+
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 80
+        path = "/"
+      }
+    }
+  }
 
 
 
